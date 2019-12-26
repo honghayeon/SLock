4fdb7847ac449777d7c8da2439b09e4835e95035
honghayeon <41175122+honghayeon@users.noreply.github.com>
2019-12-26 11:02:10 +0900

    server data

--- SLock/app/src/main/java/com/example/slock/SignInActivity.java
+++ SLock/app/src/main/java/com/example/slock/SignInActivity.java
@@ -2,7 +2,6 @@ package com.example.slock;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +12,22 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class SignInActivity extends AppCompatActivity {
+
+    final RequestQueue queue = Volley.newRequestQueue(SignInActivity.this);
 
     Button signupBtn, continueBtn;
     EditText inId, inPwd; // 로그인하기 위해 입력한 id와 pwd
@@ -43,10 +57,55 @@ public class SignInActivity extends AppCompatActivity {
 //                Intent intent = new Intent(getApplicationContext(), MainTeacherActivity.class);
 //                startActivity(intent);
 
-                ToastCustom("로그인 성공");
+                if (inId.getText().toString().equals("")) {
+                    ToastCustom("ID를 입력해주세요.");
+                    return;
+                }
+                if (inPwd.getText().toString().equals("")) {
+                    ToastCustom("비밀번호를 입력해주세요");
+                    return;
+                }
+                String url = "http://10.120.74.188:8080/login";
+                HashMap<String, String> data = new HashMap<>();
+                data.put("id", inId.getText().toString());
+                data.put("pw", inPwd.getText().toString());
+                // Request a string response from the provided URL
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(data),
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONObject jsonObject = new JSONObject(response.toString());
+                                    String result=jsonObject.getString("status");
+                                    if(result.equals("students")){
+                                        ToastCustom("로그인 성공");
+                                        // ToastCustom(resultName +  "님 로그인을 환영합니다!");
 
-                Intent intent = new Intent(getApplicationContext(), MainStudentActivity.class);
-                startActivity(intent);
+                                        Intent intent = new Intent(getApplicationContext(), MainStudentActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    else if(result.equals("teacher")){
+                                        ToastCustom("로그인 성공");
+
+                                        Intent intent = new Intent(getApplicationContext(), MainTeacherActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(), "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(getApplicationContext(), "서버가 꺼져있습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                );
+                request.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                queue.add(request);
             }
         });
     }
--- SLock/app/src/main/java/com/example/slock/SignUpTeacherActivity.java
+++ SLock/app/src/main/java/com/example/slock/SignUpTeacherActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class SignUpTeacherActivity extends AppCompatActivity {
+
+    final RequestQueue queue = Volley.newRequestQueue(SignUpTeacherActivity.this);
 
     Button signinBtn, signupBtn, continueBtn;
     EditText tName, tId, tPwd1,tPwd2, tCertnum; // 회원가입할 선생님 이름, 인증번호
@@ -48,10 +67,86 @@ public class SignUpTeacherActivity extends AppCompatActivity {
         continueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 선생님 회원가입 페이지의 continue가 눌리면
+                if (tName.getText().toString().equals("")) {
+                    ToastCustom("이름을 입력해주세요");
+                    return;
+                }
+                if (tId.getText().toString().equals("")) {
+                    ToastCustom("ID를 입력해주세요.");
+                    return;
+                }
+                if (tPwd1.getText().toString().equals("")) {
+                    ToastCustom("비밀번호를 입력해주세요");
+                    return;
+                }
+                if (tPwd2.getText().toString().equals("")) {
+                    ToastCustom("비밀번호를 다시 한 번 입력해주세요");
+                    return;
+                }
+                if (tCertnum.getText().toString().equals("")) {
+                    ToastCustom("인증번호를 입력해주세요");
+                    return;
+                }
 
-                startActivity(intent);
+                if(tPwd1.getText().toString().equals(tPwd2.getText().toString())){
+                    // ************꼭 URL 수정할 것*************
+                    String url = "http://10.120.74.188:8080/login";
+                    // ************꼭 URL 수정할 것*************
+                    HashMap<String, String> data = new HashMap<>();
+                    data.put("name", tName.getText().toString());
+                    data.put("id", tId.getText().toString());
+                    data.put("pw", tPwd1.getText().toString());
+                    data.put("cnum", tCertnum.getText().toString());
+                    // Request a string response from the provided URL
+                    JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(data),
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    try {
+                                        JSONObject jsonObject = new JSONObject(response.toString());
+                                        String result=jsonObject.getString("status");
+                                        if(result.equals("ok")){
+                                            ToastCustom("회원가입 성공");
+                                            // ToastCustom(resultName +  "님 회원가입을 환영합니다!");
+
+                                            finish();
+                                        }
+                                        else{
+                                            ToastCustom("인증번호가 일치하지 않습니다.");
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(getApplicationContext(), "서버가 꺼져있습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    );
+                    request.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                    queue.add(request);
+                }
+                else{
+                    ToastCustom("비밀번호가 일치하지 않습니다.");
+                }
             }
         });
+    }
+
+    public void ToastCustom(String word){
+        LayoutInflater inflater = getLayoutInflater();
+
+        View layout = inflater.inflate(R.layout.toastborder, (ViewGroup)findViewById(R.id.toast_layout_root));
+        TextView text = layout.findViewById(R.id.toastTxt);
+
+        Toast toast = new Toast(getApplicationContext());
+        text.setText(word);
+//        toast.setGravity(Gravity.CENTER, 0, 500);
+        toast.setDuration(Toast.LENGTH_SHORT);
+
+        toast.setView(layout);
+        toast.show();
     }
 }
